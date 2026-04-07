222d69fb467530a133f866df29306dbf78dc3210
BimoFr <bimofadhlurrahman@gmail.com>
2026-04-07 10:24:05 +0700

    Menambahkan logika untuk menghitung dan menampilkan status kelulusan berdasarkan nilai rata-rata

--- not_logic.cpp
+++ not_logic.cpp
@@ -9,5 +9,17 @@ int main()
     cout<<"Masukkan Nilai Matematika = ";
     cin >> nilM;
     cout<<"Masukkan Nilai Bahasa Inggris = ";
-    cin >> nilB;   
+    cin >> nilB;
+    
+    rerata = (nilM + nilB) / 2;
+
+    if (!(rerata < 60))
+    {
+        status = "Lulus";
+    }
+    else
+    {
+        status = "Tidak Lulus";
+    }
+    cout<<"Status Kelulusan = "<<status<<" , dengan nilai rata-rata = "<<rerata<<endl;
 }
