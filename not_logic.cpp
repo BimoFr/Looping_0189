85729b19e6dce47faf230c0b3ca91a208171e837
BimoFr <bimofadhlurrahman@gmail.com>
2026-04-07 10:22:07 +0700

    Menambahkan implementasi fungsi utama untuk menerima input nilai Matematika dan Bahasa Inggris

--- not_logic.cpp
+++ not_logic.cpp
@@ -1,3 +1,13 @@
 #include <iostream>
 using namespace std;
 
+int main()
+{
+    float nilB, nilM, rerata;
+    string status;
+
+    cout<<"Masukkan Nilai Matematika = ";
+    cin >> nilM;
+    cout<<"Masukkan Nilai Bahasa Inggris = ";
+    cin >> nilB;   
+}
